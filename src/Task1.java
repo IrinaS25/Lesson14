79a7fa15b402803e6cdcc54701fecea4e6aa3194
IrinaS <irundsa@gmail.com>
2024-10-29 19:25:25 +0100

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+    public static void testA (){
+        System.out.println("Hello World");
+    }
+    public static void testB () {
+        System.out.println("TestB");
+    }
+}
